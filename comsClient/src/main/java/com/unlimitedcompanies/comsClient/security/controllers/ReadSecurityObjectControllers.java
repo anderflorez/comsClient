6894bb3604f75a442bf2d965e05a629838c3dd25
anderson <anderson@unlimitedcompanies.com>
2018-11-09 16:58:15 -0500

    Working on REST OAuth2

--- comsClient/src/main/java/com/unlimitedcompanies/comsClient/security/controllers/ReadSecurityObjectControllers.java
+++ comsClient/src/main/java/com/unlimitedcompanies/comsClient/security/controllers/ReadSecurityObjectControllers.java
@@ -1,156 +1,121 @@
 package com.unlimitedcompanies.comsClient.security.controllers;
 
 import java.io.IOException;
-import java.net.URLEncoder;
+import java.util.Base64;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Form;
-import javax.ws.rs.core.Response;
-
-import org.apache.commons.codec.binary.Base64;
-import org.apache.http.client.ClientProtocolException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class ReadSecurityObjectControllers
 {
 	@RequestMapping("/contacts")
-	public ModelAndView displayContacts(@RequestParam("code") String code) throws ClientProtocolException, IOException
+	public ModelAndView displayContacts(@RequestParam("code") String code) throws IOException
 	{
-		//Third attempt
-		
-		String url = "http://localhost:8080/comsws/oauth2/token";
-		String requestBody = "&code=" + code;
-			requestBody += "&client_id=comsClient";
-			requestBody += "&client_secret=somesecret";
-			requestBody += "&redirect_uri=" + URLEncoder.encode("http://localhost:8080/coms/contacts", "UTF-8");
-			requestBody += "&grant_type=authorization_code";
-			
-		Form form = new Form();
-		form.param("client_id", "comsClient");
-		form.param("client_secret", "somesecret");
-		form.param("code", code);
-		form.param("grant_type", "authorization_code");
-		form.param("redirect_uri", URLEncoder.encode("http://localhost:8080/coms/contacts", "UTF-8"));
-//		form.param("scope", "Scope1");
-			
-		String credentials = "comsClient:somesecret";
-		String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
-		
-		Client client = ClientBuilder.newClient();
-		WebTarget tokenRequestTarget = client.target(url);
-		
-		Response response = tokenRequestTarget
-				.request()
-//				.header("Authorization", "Basic " + encodedCredentials)
-				.post(Entity.form(form));
-		
-		System.out.println("=============================================");
-		System.out.println(response);
-		System.out.println("=============================================");
+		//Required parameters in method - @RequestParam("code") String code
 		
 		
 		
 		
-		
-//		//Second Attempt
-//		String url = "http://localhost:8080/comsws/oauth/token";
-////		url += "?grant_type=authorization_code";
-////		url += "&code=" + code;
-////		url += "&redirect_uri=http://localhost:8080/coms/contacts";
-////		url += "&client_id=comsClient";
+//		//Third attempt
 //		
+//		String url = "http://localhost:8080/comsws/oauth2/token";
+//		String requestBody = "&code=" + code;
+//			requestBody += "&client_id=comsClient";
+//			requestBody += "&client_secret=somesecret";
+//			requestBody += "&redirect_uri=" + URLEncoder.encode("http://localhost:8080/coms/contacts", "UTF-8");
+//			requestBody += "&grant_type=authorization_code";
+//			
+//		Form form = new Form();
+//		form.param("client_id", "comsClient");
+//		form.param("client_secret", "somesecret");
+//		form.param("code", code);
+//		form.param("grant_type", "authorization_code");
+//		form.param("redirect_uri", URLEncoder.encode("http://localhost:8080/coms/contacts", "UTF-8"));
+////		form.param("scope", "Scope1");
+//			
 //		String credentials = "comsClient:somesecret";
 //		String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
 //		
-//		List<NameValuePair> params = new ArrayList<>();
-//		params.add(new BasicNameValuePair("grant_type", "authorization_code"));
-//		params.add(new BasicNameValuePair("code", code));
-//		params.add(new BasicNameValuePair("redirect_uri", "http://localhost:8080/coms/contacts"));
-//		params.add(new BasicNameValuePair("client_id", "comsClient"));
-//		params.add(new BasicNameValuePair("Authorization", "Basic " + encodedCredentials));
+//		Client client = ClientBuilder.newClient();
+//		WebTarget tokenRequestTarget = client.target(url);
 //		
-//		HttpClient httpClient = HttpClients.createDefault();
-//		HttpPost httpPost = new HttpPost(url);
-//		httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-//		
-//		HttpResponse response = httpClient.execute(httpPost);
-//		HttpEntity entity = response.getEntity();
-//		
-//		if (entity != null)
-//		{
-//			try (InputStream instream = entity.getContent())
-//			{
-//				System.out.println("=============================================");
-//				System.out.println(instream);
-//				Reader reader = new InputStreamReader(instream);
-//				BufferedReader br = new BufferedReader(reader);
-//				System.out.println(br.readLine());
-//				
-//			} catch (UnsupportedOperationException e)
-//			{
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			} catch (IOException e)
-//			{
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-//		ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-//		
-//		
-//		
-//		
-//		RestTemplate template = new RestTemplate(requestFactory);
-//		
-//		String credentials = "comsClient:somesecret";
-//		String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes())); //Base64.getEncoder().encodeToString(credentials.getBytes());
-//		
-//		List<MediaType> acceptableMediaTypes = new ArrayList<>();
-//		acceptableMediaTypes.add(MediaType.APPLICATION_XML);
-//		acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
-//		
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.setAccept(acceptableMediaTypes);
-//		headers.add("Authorization", "Basic " + encodedCredentials);
-//		HttpEntity<String> request = new HttpEntity<>(headers);
-//		
-//		System.out.println("=================> Headers: " + request.getHeaders());
-//		
-//		String url = "http://localhost:8080/comsws/oauth/token";
-//		url += "?grant_type=authorization_code";
-//		url += "&code=" + code;
-//		url += "&redirect_uri=http://localhost:8080/coms/contacts";
-//		url += "&client_id=comsClient";
-//		
-//		System.out.println("=================> url: " + url);
-//		
-//		ResponseEntity<String> response = template.exchange(url, HttpMethod.POST, request, String.class);
+//		Response response = tokenRequestTarget
+//				.request()
+////				.header("Authorization", "Basic " + encodedCredentials)
+//				.post(Entity.form(form));
 //		
 //		System.out.println("=============================================");
-//		System.out.println("\n\n\n" + response.getBody() + "\n\n\n");
-		System.out.println("=============================================");
-		System.out.println("Finished running the client");
+//		System.out.println(response);
+//		System.out.println("=============================================");
 		
-		return null;
+		
+		
+		
+		
+		RestTemplate template = new RestTemplate();
+		template.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+		
+		String credentials = "comsClient:somesecret";
+		String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
+		
+		HttpHeaders headers1 = new HttpHeaders();
+		headers1.add("Authorization", "Basic " + encodedCredentials);
+		
+		HttpEntity<String> request = new HttpEntity<>(headers1);
+		
+		String url = "http://localhost:8080/comsws/oauth/token";
+		url += "?code=" + code;
+		url += "&grant_type=authorization_code";
+		url += "&redirect_uri=http://localhost:8080/coms/contacts";
+		url += "&client_id=comsClient";
+		
+		System.out.println("\n\n========> Headers before request: \n" + request + "\n\n");
+	
+		ResponseEntity<String> response = template.exchange(url, HttpMethod.POST, request, String.class);
+		
+		System.out.println("\n\n\n" + response.getBody() + "\n\n\n");
+		
+		
+		
+		
+		url = "http://localhost:8080/comsws/rest/contacts";
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = mapper.readTree(response.getBody());
+		String token = node.path("access_token").asText();
+		
+		HttpHeaders headers2 = new HttpHeaders();
+		headers2.add("Authorization", "Bearer " + token);
+		HttpEntity<String> contactRequest = new HttpEntity<String>(headers2);
+		
+		System.out.println("\n\n========> url request: \n" + url);
+		System.out.println("========> headers before request: \n" + contactRequest + "\n\n");
+		
+
+		try
+		{
+			ResponseEntity<String> contactsResponseEntity = template.exchange(url, HttpMethod.GET, contactRequest, String.class);
+			System.out.println("\n\n\n" + contactsResponseEntity.getBody() + "\n\n\n");
+
+		} catch (OAuth2Exception e)
+		{
+			System.out.println("\n\n========> Exception occurred:\n");
+			System.out.println(e.getMessage());
+//			e.printStackTrace();
+		}
+				
+		return null;//new ModelAndView("/importedContacts.jsp", "contacts", contactsResponseEntity.getBody().getContacts());
 	}
 }
